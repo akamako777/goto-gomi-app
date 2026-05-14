3cebfdc8f915a248d242bc48e089d48c7d2f5432
makoto akao <akamako.777.epicday@gmail.com>
2026-05-14 10:18:45 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,3 +1,26 @@
+// Firebaseの裏方専用ライブラリを読み込み
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js');
+
+// ★取得した1つ目のカギ（設定コード）
+firebase.initializeApp({
+  apiKey: "AIzaSyDDi3IdcvyBh4R3nh7_bMQkabpWfcZpPFk",
+  authDomain: "goto-gomi-app.firebaseapp.com",
+  projectId: "goto-gomi-app",
+  storageBucket: "goto-gomi-app.firebasestorage.app",
+  messagingSenderId: "730896181153",
+  appId: "1:730896181153:web:eeaa805011a4a807250de0"
+});
+
+// バックグラウンドで通知を受け取る準備
+const messaging = firebase.messaging();
+
+// ★追加: アプリが閉じている時に通知を受け取るための処理
+messaging.onBackgroundMessage((payload) => {
+  console.log('[firebase-messaging-sw.js] バックグラウンド通知を受信:', payload);
+});
+
+
 // sw.js (Adom's Lab 統合版 v12: FontAwesome JS対応ハイブリッド版)
 // ローカルファイル(JS)とCDN(CSS/Webfonts)、どちらを使っていてもキャッシュするように設定しています
 
